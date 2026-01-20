f35623995d0adfaaa5b74ca2c1aa18319135bd78
Ahaan <ahaan.basumojumdar@gmail.com>
2026-01-20 08:46:42 +0530

    modified:   W1_CW_03_first_integer.c 	modified:   W1_HW_01_print_statement.c 	modified:   W1_HW_02_Variables_Int_Float.c 	modified:   a.out

--- W1_CW_03_first_integer.c
+++ W1_CW_03_first_integer.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 int main()
 {
@@ -5,5 +6,18 @@ int main()
     int b = 8;
     int c = 9;
     printf("The value of a is %d",a+b+c);
+    return 0;
+}
+*/
+
+#include <stdio.h>
+int main()
+
+{
+    int qty = 5;
+    float price = 5.5;
+
+    printf("The Price of %d bananas is %.1f",qty,qty*price);
+
     return 0;
 }
---
+++ W1_HW_02_Variables_Int_Float.c
@@ -0,0 +1,66 @@
+/*
+//Write a program to print:
+//I have 10 chocolates in 5 packets.
+//Each costs Rs. 1.5.
+
+#include <stdio.h>
+int main()
+{
+    int qty_of_items = 10;
+    char item[] = "chocolates";
+    int qty_of_packets = 5;
+    char packet[] = "packets";
+    float cost = 1.5;
+
+    printf("I have %d %s in %d %s.\n",qty_of_items,item,qty_of_packets,packet);
+    printf("Each costs Rs. %.1f.\n",cost);
+
+    return 0;
+}
+
+//Write a program to show your age using a variable.
+//Example output:
+//I am 9 years old
+
+#include<stdio.h>
+int main()
+{
+    int age = 9;
+
+    printf("I am %d years old.\n",age);
+
+    return 0;
+}
+
+//Write a program to store your height in a float variable and print it.
+//Example output:
+//My height is 4.5
+
+#include<stdio.h>
+int main()
+{
+    float height = 4.5;
+
+    printf("I am %.1f years old.\n",height);
+
+    return 0;
+}
+
+//Write a program to store number of apples = 5 and price = 2.5, then print both.
+//Example output:
+//Apples = 5
+//Price = 2.5
+
+#include <stdio.h>
+int main()
+{
+    int qty = 5;
+    float price = 2.5;
+
+    printf("Apples = %d\n",qty);
+    printf("Price = %0.1f\n",price);
+
+    return 0;
+}
+
+*/
