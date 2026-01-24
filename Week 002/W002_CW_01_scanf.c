11f358dc1bc144a69c51f8688e2c229b03e719ce
Ahaan <ahaan.basumojumdar@gmail.com>
2026-01-24 13:29:03 +0530

    modified:   .DS_Store 	new file:   Week 002/W002_CW_01_scanf 	modified:   Week 002/W002_CW_01_scanf.c 	new file:   Week 002/a.out

--- Week 002/W002_CW_01_scanf.c
+++ Week 002/W002_CW_01_scanf.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 int main()
 {
     int a;
     char name[30];
+    
     printf("Enter a number: ");
     scanf("%d",&a);
     printf("The value entered by the user is %d.\n",a);
+    
     printf("Enter a name: ");
     scanf("%s", name);
     printf("The users name is %s.\n", name);
+    
     return 0;
 }
